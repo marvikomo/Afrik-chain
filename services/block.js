170cd598c4783f5cd368f41fe86828a9667e5b13
erindeji <erinfolami.rasaq@konga.com>
2021-05-11 17:49:01 +0100

    change app to have services and routes separated

---
+++ services/block.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const Blockchain = require("../blockchain");
+const P2PServer = require("../app/p2p-server");
+
+const blockchain = new Blockchain();
+const p2pServer = new P2PServer(blockchain);
+
+exports.blocks = async (req, res, next) => {
+  res.json(blockchain.chain);
+};
+
+exports.mine = async (req, res, next) => {
+  const block = blockchain.addBlock(req.body.data);
+  console.log("new block added");
+
+  p2pServer.syncChains();
+
+  res.redirect("/api/v1/work");
+};
